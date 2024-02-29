ed22bb4d55f67e45e22a32098d6f1a9ba1e8384d
lsc713 <lsc7134.naver.com>
2024-02-29 12:14:08 +0900

    [ADD] init Commit

---
+++ HH99SpringLv3/src/main/java/com/sparta/hh99springlv3/Hh99SpringLv3Application.java
@@ -0,0 +1,13 @@
+package com.sparta.hh99springlv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hh99SpringLv3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hh99SpringLv3Application.class, args);
+    }
+
+}
---
+++ HH99SpringLv3/src/test/java/com/sparta/hh99springlv3/Hh99SpringLv3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.hh99springlv3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hh99SpringLv3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
