19cfea733b9cdaad2e8bd318ea0947de7d59c9bd
VanMao <139448668+lsc713@users.noreply.github.com>
2024-02-29 20:32:30 +0900

    feature : [ADD]Global ExceptionHandler body : 예외처리와 관련내용 추가 footer : #11

---
+++ HH99SpringLv3/src/main/java/com/sparta/hh99springlv3/global/config/CustomValidationAdvice.java
@@ -0,0 +1,43 @@
+package com.sparta.hh99springlv3.global.config;
+
+import com.sparta.hh99springlv3.global.handler.exception.CustomValidationException;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Aspect
+@Component
+public class CustomValidationAdvice {
+
+    @Pointcut("@annotation(org.springframework.web.bind.annotation.PostMapping)")
+    public void postMapping() {
+
+    }
+
+    @Pointcut("@annotation(org.springframework.web.bind.annotation.PutMapping)")
+    public void putMapping() {
+
+    }
+
+    @Around("postMapping() || putMapping()")
+    public Object validationAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        Object[] args = proceedingJoinPoint.getArgs();
+        for (Object arg : args) {
+            if (arg instanceof BindingResult bindingResult && (bindingResult.hasErrors())) {
+                Map<String, String> errorMap = new HashMap<>();
+                for (FieldError error : bindingResult.getFieldErrors()) {
+                    errorMap.put(error.getField(), error.getDefaultMessage());
+                }
+                throw new CustomValidationException("유효성 검사 실패", errorMap);
+            }
+        }
+        return proceedingJoinPoint.proceed();
+    }
+}
---
+++ HH99SpringLv3/src/main/java/com/sparta/hh99springlv3/global/handler/exception/CustomApiException.java
@@ -0,0 +1,11 @@
+package com.sparta.hh99springlv3.global.handler.exception;
+
+import lombok.Getter;
+
+@Getter
+public class CustomApiException extends RuntimeException {
+
+    public CustomApiException(String message) {
+        super(message);
+    }
+}
