c0f9596d44d43c724fe9f9762056f947ab87e811
VanMao <139448668+lsc713@users.noreply.github.com>
2024-02-29 14:29:59 +0900

    docs : [ADD] body : repository jpa 연결 및 entity 추가 footer : VanMao
    (cherry picked from commit 130395aabef822aeb01cd7e5f3091caaf2bfc1a5)

---
+++ HH99SpringLv3/src/main/java/com/sparta/hh99springlv3/domain/admin/repository/AdminRepository.java
@@ -0,0 +1,7 @@
+package com.sparta.hh99springlv3.domain.admin.repository;
+
+import com.sparta.hh99springlv3.domain.admin.entity.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminRepository extends JpaRepository<Admin,Long> {
+}
