7f7be2a216a4452d5a215a99a394784d292c6a86
lsc713 <lsc7134.naver.com>
2024-03-01 19:06:40 +0900

    chore : [ADD] Entity추가 body : 로그인시 필요한 내용 추가 footer : #2

--- HH99SpringLv3/src/main/java/com/sparta/hh99springlv3/domain/lecture/entity/Lecture.java
+++ HH99SpringLv3/src/main/java/com/sparta/hh99springlv3/domain/lecture/entity/Lecture.java
@@ -1,37 +1,25 @@
 package com.sparta.hh99springlv3.domain.lecture.entity;
 
-import com.sparta.hh99springlv3.global.entity.Timestamped;
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 @Entity
-@Builder
-@AllArgsConstructor
-public class Lecture extends Timestamped {
+public class Lecture {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false,unique = true)
-    private String lectureName;
-    @Column(nullable = false)
+    private String LectureName;
     private int price;
-    @Column(nullable = false)
     private String lecIntroduce;
-
-    @Column(nullable = false)
-    @Enumerated(EnumType.STRING)
-    private LCareer category;
-
-    @Column(nullable = false)
-    private String tutor;
-
+    private String category;
 
 
 }
