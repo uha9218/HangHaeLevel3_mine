574bb9e991fce66c8fc52fdfaf257724f4ec4c4b
lsc713 <lsc7134.naver.com>
2024-03-02 15:46:29 +0900

    docs:[ADD] swagger설정 및 테스트 완료 body:swagger설정 및 테스트 완료

--- HH99SpringLv3/src/main/java/com/sparta/hh99springlv3/domain/lecture/controller/LectureController.java
+++ HH99SpringLv3/src/main/java/com/sparta/hh99springlv3/domain/lecture/controller/LectureController.java
@@ -6,6 +6,11 @@ import com.sparta.hh99springlv3.domain.lecture.entity.Category;
 import com.sparta.hh99springlv3.domain.lecture.service.LectureService;
 import com.sparta.hh99springlv3.global.entity.AuthEnum;
 import com.sparta.hh99springlv3.global.jwt.JwtUtil;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -22,11 +27,14 @@ import static com.sparta.hh99springlv3.domain.lecture.dto.LectureResponseDto.Get
 @RestController
 @RequestMapping("/api/v1/lectures")
 @RequiredArgsConstructor
+@Tag(name = "Lecture API",description = "강의 관련 API 내용입니다.")
 public class LectureController {
 
     private final LectureService lectureService;
 
     @PostMapping("/")
+    @Operation(summary = "createLecture",description = "관리자가 강의를 등록한다.")
+    @ApiResponse(responseCode = "201", description = "강의 등록 성공", content = @Content(schema = @Schema(implementation = CreateLectureResponseDto.class)))
     public ResponseEntity<CreateLectureResponseDto> createLecture(@RequestBody @Valid CreateLectureRequestDto requestDto,
                                                                   @CookieValue(JwtUtil.AUTHORIZATION_HEADER) String tokenValue) {
         CreateLectureResponseDto responseDto = lectureService.createLecture(requestDto, tokenValue);
@@ -35,6 +43,8 @@ public class LectureController {
 
     @Secured(AuthEnum.Authority.MANAGER)
     @PutMapping("/{lectureId}")
+    @Operation(summary = "updateLecture",description = "매니저가 강의를 수정한다, Manager권한 필요")
+    @ApiResponse(responseCode = "200", description = "강의 수정 성공", content = @Content(schema = @Schema(implementation = GetLectureResponseDto.class)))
     public ResponseEntity<GetLectureResponseDto> updateLecture(@PathVariable Long lectureId, @RequestBody UpdateLectureRequestDto requestDto,
                                                                @CookieValue(JwtUtil.AUTHORIZATION_HEADER) String tokenValue) {
         GetLectureResponseDto responseDto = lectureService.updateLecture(lectureId, requestDto, tokenValue);
@@ -42,6 +52,8 @@ public class LectureController {
     }
 
     @GetMapping("/get/{lectureId}")
+    @Operation(summary = "getLecture",description = "카테고리별 강의를 조회한다.")
+    @ApiResponse(responseCode = "200", description = "카테고리별 강의 조회 성공", content = @Content(schema = @Schema(implementation = GetLectureResponseDto.class)))
     public ResponseEntity<GetLectureResponseDto> getLecture(@PathVariable Long lectureId,
                                                             @CookieValue(JwtUtil.AUTHORIZATION_HEADER) String tokenValue
     ) {
@@ -50,6 +62,8 @@ public class LectureController {
     }
 
     @GetMapping("/category/{category}")
+    @Operation(summary = "getLectureCategory",description = "관리자가 강의를 등록한다.")
+    @ApiResponse(responseCode = "200", description = "카테고리별 강의 조회 성공", content = @Content(schema = @Schema(implementation = List.class)))
     public ResponseEntity<List<GetLectureResponseDto>> getLectureCategory(@PathVariable Category category,
                                                                           @CookieValue(JwtUtil.AUTHORIZATION_HEADER) String tokenValue) {
 
@@ -58,12 +72,16 @@ public class LectureController {
     }
 
     @GetMapping("/tutors/{tutor}")
+    @Operation(summary = "getLectureByTutor",description = "강사별 강의를 조회한다.")
+    @ApiResponse(responseCode = "200", description = "강사별 강의 조회 성공", content = @Content(schema = @Schema(implementation = List.class)))
     public ResponseEntity<List<GetLectureResponseDto>> getLectureByTutor(@PathVariable Long tutor, @CookieValue(JwtUtil.AUTHORIZATION_HEADER) String tokenValue) {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(lectureService.getLectureByTutor(tutor, tokenValue));
     }
 
     @DeleteMapping("/{lectureId}")
+    @Operation(summary = "deleteLecture",description = "매니저가 강의를 삭제한다, Manager권한 필요")
+    @ApiResponse(responseCode = "200", description = "강의 삭제 성공", content = @Content(schema = @Schema(implementation = Long.class)))
     public ResponseEntity<Long> deleteLecture(@PathVariable Long lectureId, @CookieValue(JwtUtil.AUTHORIZATION_HEADER) String tokenValue) {
         return ResponseEntity.ok()
                 .body(lectureService.deleteLecture(lectureId, tokenValue));
--- HH99SpringLv3/src/main/java/com/sparta/hh99springlv3/domain/tutor/dto/TutorRequestDto.java
+++ HH99SpringLv3/src/main/java/com/sparta/hh99springlv3/domain/tutor/dto/TutorRequestDto.java
@@ -1,18 +1,28 @@
 package com.sparta.hh99springlv3.domain.tutor.dto;
 
 import com.sparta.hh99springlv3.domain.tutor.entity.Tutor;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 public class TutorRequestDto {
     @Getter
     public static class CreateTutorRequestDto {
+        @Schema(description = "강사 이름", example = "홍길동")
         @NotBlank(message = "이름을 입력해주세요.")
         private String tutorName;
+
+        @Schema(description = "강사 경력", example = "20")
         private String career;
+
+        @Schema(description = "소속 회사", example = "금성 전자")
         private String company;
+
+        @Schema(description = "전화번호", example = "010-1234-5678")
         @NotBlank(message = "전화번호를 입력해주세요.")
         private String phoneNumber;
+
+        @Schema(description = "강사 소개", example = "세상을 널리 이롭게 하는 강사")
         private String tuIntroduce;
 
         public Tutor toEntity() {
@@ -28,9 +38,16 @@ public class TutorRequestDto {
 
     @Getter
     public static class UpdateTutorRequestDto {
+        @Schema(description = "강사 경력", example = "20")
         private String career;
+
+        @Schema(description = "소속 회사", example = "금성 전자")
         private String company;
+
+        @Schema(description = "전화번호", example = "010-1234-5678")
         private String phoneNumber;
+
+        @Schema(description = "강사 소개", example = "세상을 널리 이롭게 하는 강사")
         private String tuIntroduce;
     }
 
